ef6de405a815b7c3e86111b3c2df7a4c6b50f4c7
arunattri26 <5784970+arunattri26@users.noreply.github.com>
2025-09-09 15:28:46 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -1,13 +1,4 @@
-/**
- * Get Full Name
- * @name getFullName Concats first name and last name
- * @param {string} firstname in Stringformat
- * @param {string} lastname in Stringformat
- * @return {string}
- */
-function getFullName(firstname, lastname) {
-  return `${firstname} ${lastname}`.trim();
-}
+import { getFullName1 } from './commonFunctions.js';
 
 /**
  * Custom submit function
