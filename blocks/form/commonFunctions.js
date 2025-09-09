4c1432a7799de0b007281630f82dbb3ed9e203ee
arunattri26 <5784970+arunattri26@users.noreply.github.com>
2025-09-09 15:27:55 +0530

    Create commonFunctions.js

---
+++ blocks/form/commonFunctions.js
@@ -0,0 +1,12 @@
+/**
+ * Get Full Name
+ * @name getFullName Concats first name and last name
+ * @param {string} firstname in Stringformat
+ * @param {string} lastname in Stringformat
+ * @return {string}
+ */
+function getFullName(firstname, lastname) {
+  return `${firstname} ${lastname}`.trim();
+}
+
+export {getFullName};
