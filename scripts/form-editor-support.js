f82dc709651b156b04c7f46559f69ce9fa1cfc23
Arun Kumar Attri <aattri@adobe.com>
2025-09-11 11:13:29 +0530

    FORMS-19946: testing

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -360,7 +360,7 @@ function enableRuleEditorExtension() {
   const params = new URLSearchParams(window.location.search);
   console.log('Rule editor extension Params: ', params);
   let version = params.get('livecycle-ruleeditor-ui-service_version');
-  version = 'PR-261-a74c1978e15b87e73da33e3aea28878801e0976a';
+  version = 'PR-261-e7c130dc0216e5f54c61d0aed6473dc5062820b0';
   const domain = params.get('livecycle-ruleeditor-ui-service_domain') || 'https://experience.adobe.com';
   if (version) {
     meta.content = `${domain}/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html?livecycle-ruleeditor-ui-service_version=${version}`;
